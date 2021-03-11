ca2cd25990ef48ace035b92c451a3e1a20cc43c8
Stanislau Kharlap <stanislau.kharlap@multisafepay.com>
2021-03-11 15:32:25 +0100

    rename the package files

--- src/component/Ideal/Ideal.style.scss
+++ src/component/Ideal/Ideal.style.scss
@@ -3,7 +3,7 @@
  * See DISCLAIMER.md for disclaimer details.
  *
  * @license OSL-3.0 (Open Software License ("OSL") v. 3.0)
- * @package multisafepay-integration
+ * @package scandipwa-multisafepay-payment-integration
  * @link https://github.com/MultiSafepay/scandipwa-multisafepay-payment-integration
  *
  */
